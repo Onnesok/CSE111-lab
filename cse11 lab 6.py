110c016df99545cdafa52ed58eac59ee3d124845
Onnesok <ratul.hasan@g.bracu.ac.bd>
2022-08-10 15:54:50 +0600

    init

--- cse11 lab 6.py
+++ cse11 lab 6.py
@@ -41,29 +41,135 @@
 # #You are not allowed to change the code above
 
 ############################  Task 6  ###########################
-class Laptop:
-    laptopCount = 0
-    def __init__(self, name, count):
-        self.name = name
-        self.count = count
-        Laptop.laptopCount += self.count
+# class Laptop:
+#     laptopCount = 0
+#     def __init__(self, name, count):
+#         self.name = name
+#         self.count = count
+#         Laptop.laptopCount += self.count
 
-    @staticmethod
-    def advantage():
-        print("Laptops are portable")
-    @classmethod
-    def resetCount(cls):
-        Laptop.laptopCount = 0
+#     @staticmethod
+#     def advantage():
+#         print("Laptops are portable")
+#     @classmethod
+#     def resetCount(cls):
+#         Laptop.laptopCount = 0
 
-# Write your code here
-lenovo = Laptop("Lenovo", 5);
-dell = Laptop("Dell", 7);
-print(lenovo.name, lenovo.count)
-print(dell.name, dell.count)
-print("Total number of Laptops", Laptop.laptopCount)
-Laptop.advantage()
-Laptop.resetCount()
-print("Total number of Laptops", Laptop.laptopCount)
+# # Write your code here
+# lenovo = Laptop("Lenovo", 5);
+# dell = Laptop("Dell", 7);
+# print(lenovo.name, lenovo.count)
+# print(dell.name, dell.count)
+# print("Total number of Laptops", Laptop.laptopCount)
+# Laptop.advantage()
+# Laptop.resetCount()
+# print("Total number of Laptops", Laptop.laptopCount)
 
 #############################  Task 10  ##########################
+# class SultansDine:
+#     def __init__(self, place, taka = 10000):
+#         self.place = place
+#         self.taka = taka
+#     def details(self, branch = 0, taka = 0):
+#         self.branch = branch
+#         self.taka = taka
+#         print("Total number of branch(s):", self.branch, "\nTotal Sell:", self.taka, "Taka")
 
+#     def branchInformation(self):
+#         print("Branch Name:", self.place, "\nBranch sell:", self.taka, "Taka")
+
+#     def sellQuantity(self, num):
+#         self.num = num
+# Write your code here
+# SultansDine.details()
+# print('########################')
+# dhanmodi = SultansDine('Dhanmondi')
+# dhanmodi.sellQuantity(25)
+# dhanmodi.branchInformation()
+# print('-----------------------------------------')
+# SultansDine.details()
+# print('========================')
+# baily_road = SultansDine('Baily Road')
+# baily_road.sellQuantity(15)
+# baily_road.branchInformation()
+# print('-----------------------------------------')
+# SultansDine.details()
+# print('========================')
+# gulshan = SultansDine('Gulshan')
+# gulshan.sellQuantity(9)
+# gulshan.branchInformation()
+# print('-----------------------------------------')
+# SultansDine.details()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SultansDine:
+    total_sell = 0
+    total_branch = 0
+    info = []
+
+    def __init__(self, place):
+        self.place = place
+        self.quantity = 0
+        SultansDine.total_branch += 1
+        SultansDine.info.append([self.place])
+
+    def sellQuantity(self, quantity):
+        self.quantity = quantity
+
+    def branchInformation(self):
+        total = 0
+        if self.quantity < 10:
+            total = self.quantity * 300
+        elif self.quantity < 20:
+            total = self.quantity * 350
+        else:
+            total = self.quantity * 400
+        SultansDine.total_sell += total
+        SultansDine.info[SultansDine.total_branch - 1] += [total]
+        print(f"Branch Name: {self.place}\nBranch Sell: {total} Taka")
+
+    @classmethod
+    def details(cls):
+        print(f"Total Number of branch(s): {SultansDine.total_branch}\nTotal Sell: {SultansDine.total_sell} Taka")
+        for b, s in cls.info:
+            print(f"Branch Name: {b}, Branch Sell: {s} Taka")
+            print(f"Branch consists of total sell's: {round(s / SultansDine.total_sell * 100, 2)}%")
+
+    # Driver class...
+
+
+SultansDine.details()
+print('########################')
+dhanmodi = SultansDine('Dhanmondi')
+dhanmodi.sellQuantity(25)
+dhanmodi.branchInformation()
+print('-----------------------------------------')
+SultansDine.details()
+print('========================')
+baily_road = SultansDine('Baily Road')
+baily_road.sellQuantity(15)
+baily_road.branchInformation()
+print('-----------------------------------------')
+SultansDine.details()
+print('========================')
+gulshan = SultansDine('Gulshan')
+gulshan.sellQuantity(9)
+gulshan.branchInformation()
+print('-----------------------------------------')
+SultansDine.details()
